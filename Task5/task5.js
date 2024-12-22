475904953b6308b3766a98f46653a50f80577a08
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-12-22 22:38:40 +0100

    Finish Task 5

--- Task5/task5.js
+++ Task5/task5.js
@@ -1,1 +1,29 @@
 'use strict'
+
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('request', (req) => {
+    console.log("Got request");
+    console.log(`Request data: ${JSON.stringify(req)}`);
+});
+
+emitter.on('error', () => {
+    console.log("Here is an error :(");
+    emitter.emit('end')
+});
+
+emitter.on('end', () => {
+   console.log("Program ended");
+   process.exit(0);
+});
+
+emitter.emit('request', {data: {
+        name: "Anton",
+        lastName: "Kaliberda",
+        status: "logged",
+    }
+});
+
+emitter.emit('error');
